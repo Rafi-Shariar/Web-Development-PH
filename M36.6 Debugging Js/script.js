51cb2f5cb2b50c42119b115f8c52105b4405ec85
Rafi-Shariar <rafi.shariar619@gmail.com>
2025-03-22 23:36:27 +0600

    react started

---
+++ M36.6 Debugging Js/script.js
@@ -0,0 +1,13 @@
+for (let index = 0; index < 10; index++) {
+    
+    debugger; //works same as BreakPoint
+    
+    const element = index;
+    console.log(element);
+    
+}
+
+//To see debugger result -> inspect -> sources -> file_name
+//But using breakpoint is best way. To see result follow the same process
+
+ 
